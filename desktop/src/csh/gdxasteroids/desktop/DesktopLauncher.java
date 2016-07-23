b0fac2caf2fe1eaf98ad6c5e06f83adf76be83f7
Chris Henderson <chrishenderson08@gmail.com>
2016-07-23 17:00:35 -0500

    Added AUTHORS file.

--- desktop/src/csh/gdxasteroids/desktop/DesktopLauncher.java
+++ desktop/src/csh/gdxasteroids/desktop/DesktopLauncher.java
@@ -1,5 +1,5 @@
 /**
-*    Copyright 2016, Chris Henderson
+*    Copyright 2016, see AUTHORS file.
 *
 *    Licensed under the Apache License, Version 2.0 (the "License");
 *    you may not use this file except in compliance with the License.
